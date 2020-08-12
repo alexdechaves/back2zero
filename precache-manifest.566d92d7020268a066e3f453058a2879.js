d4fa7f28984b5d590bc7db75676ed7099bf43ef6
alex.dechaves <alex.dechaves@vimeo.com>
2020-08-12 16:30:43 -0400

    Updates

---
+++ precache-manifest.566d92d7020268a066e3f453058a2879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96bd06ffe6e7a322f337cd0144ce0e2",
+    "url": "/back2zero/index.html"
+  },
+  {
+    "revision": "3ddff213e9e1904215c2",
+    "url": "/back2zero/static/css/main.4c30cc7b.chunk.css"
+  },
+  {
+    "revision": "b42a2194f1343683d30a",
+    "url": "/back2zero/static/js/2.67451590.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/back2zero/static/js/2.67451590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddff213e9e1904215c2",
+    "url": "/back2zero/static/js/main.5214f527.chunk.js"
+  },
+  {
+    "revision": "5b52496fad0c416d2ad4",
+    "url": "/back2zero/static/js/runtime-main.71652148.js"
+  }
+]);
--- src/Components/Pages/Video/YTEmbed.js
+++
@@ -1,3 +0,0 @@
-import React from "react"
-import ReactPlayer from 'react-player'
- 
