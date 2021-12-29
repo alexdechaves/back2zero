805610ea6c2625af571b3ca521131a586e363af0
Alex de Chaves <39253711+alexdechaves@users.noreply.github.com>
2021-12-29 17:22:37 -0500

    Og tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,6 +7,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <title>Back 2 Zero | Official Site</title>
+          <meta property="og:url" content="https://back2zero.vercel.app/"></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:title" content="Back 2 Zero | Official Site"></meta>
+          <meta property="og:description" content=""></meta>
+          <meta property="og:image" content="/images/0036.jpeg"></meta>
         </Head>
         <body>
           <Main />
